035ca3172e87b8d3d1ffbd6b5f93a58b15d83779
van <yuzetao@chafou.com>
2023-11-20 16:21:58 +0800

    Initial Commit

---
+++ TestMethodSwizzling/TestMethodSwizzling.h
@@ -0,0 +1,18 @@
+//
+//  TestMethodSwizzling.h
+//  TestMethodSwizzling
+//
+//  Created by van on 2023/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestMethodSwizzling.
+FOUNDATION_EXPORT double TestMethodSwizzlingVersionNumber;
+
+//! Project version string for TestMethodSwizzling.
+FOUNDATION_EXPORT const unsigned char TestMethodSwizzlingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestMethodSwizzling/PublicHeader.h>
+
+
